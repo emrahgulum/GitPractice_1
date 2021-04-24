ece378f68a6a0fd36882fc7a1978cc5f8b72cd5c
emrahgulum <82914905+emrahgulum@users.noreply.github.com>
2021-04-24 16:52:46 -0400

    added one more line of code

---
+++ src/day1/HelloWorld.java
@@ -0,0 +1,10 @@
+package day1;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Adding GitHub Now");
+        System.out.println("YAY STUFFS ARE ON GITHUB");
+
+    }
+}
